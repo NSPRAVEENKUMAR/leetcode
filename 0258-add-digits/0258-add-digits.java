750f88bc0faba7642f55b47757cf801143223251
NS Praveen Kumar <praveenkumarn205@gmail.com>
2025-07-21 13:57:27 +0530

    Time: 1 ms (44.23%), Space: 40.8 MB (84.85%) - LeetHub

--- 0258-add-digits/0258-add-digits.java
+++ 0258-add-digits/0258-add-digits.java
@@ -1,6 +1,6 @@
 class Solution {
-    public int addDigits(int n) {
-        int result = n == 0 ? 0 : (n % 9 == 0 ? 9 : n % 9);
-      return result;
+    public int addDigits(int num) {
+        if (num == 0) return 0;
+        return (num % 9 == 0) ? 9 : num % 9;
     }
 }
