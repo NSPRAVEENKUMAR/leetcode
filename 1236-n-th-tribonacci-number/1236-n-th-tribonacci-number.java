3bcfe0b58833a32917e179900dc7a5ccb893ae8e
NS Praveen Kumar <praveenkumarn205@gmail.com>
2025-07-23 14:42:27 +0530

    Time: 0 ms (100%), Space: 40.7 MB (26.99%) - LeetHub

---
+++ 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int tribonacci(int n) {
+        if (n == 0) {
+            return 0;
+        } else if (n == 1 || n == 2) {
+            return 1;
+        }
+
+        int t0 = 0, t1 = 1, t2 = 1;
+        for (int i = 3; i <= n; i++) {
+            int t3 = t0 + t1 + t2;
+            t0 = t1;
+            t1 = t2;
+            t2 = t3;
+        }
+        return t2;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.tribonacci(4));  // Output: 4
+        System.out.println(solution.tribonacci(25)); // Output: 1389537
+    }
+}
